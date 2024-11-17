7aaebd2407ff9688edb7e7ab5b6a5233a4afc1d9
Selva <sellva3009@gmail.com>
2024-11-17 11:14:09 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function calculateDaysBetweenDates(begin, end) {
+  const millisecondsPerDay = 86400000;
+  const diff = end - begin;
+  return Math.floor(diff / millisecondsPerDay);
+}
+
+const today = new Date();
+
+const vacation = new Date("December 31, 2021");
+
+console.log(calculateDaysBetweenDates(today, vacation));
+
+// Path: script.js
+
+// we can use the Date object to create a new date instance
+
+const now = new Date();
+
+console.log(now);
+
+// test it out yourself!
